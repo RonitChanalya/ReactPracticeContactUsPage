5c55e28ed0752d0790a7c7db57e3bb0e9d74adae
Ronit Chanalya <ronitchanalya2005@gmail.com>
2025-11-04 11:59:19 +0530

    Added some very basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { useContext } from 'react'
 import ContactUs from './components/pages/ContactUs.jsx'
 import Response from './components/pages/Response.jsx'
 import { contactContext } from './components/Context/FormContext.jsx'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
 
   const { formData } = useContext(contactContext)
-  console.log(formData);
 
   return (
     <>
-      <div className='flex flex-nowrap items-center justify-center h-screen w-full bg-[#ffe6e9]'>
+
+    <Routes>
+      <Route path='/login' element={<div className='flex flex-nowrap items-center justify-center h-screen w-full bg-[#ffe6e9]'>
         { formData === null ? <ContactUs /> : <Response /> }
-      </div> 
+      </div>} />
+    </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import FormContext from './components/Context/FormContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <FormContext>
-    <App />
-  </FormContext>
+  <BrowserRouter>
+    <FormContext>
+      <App />
+    </FormContext>
+  </BrowserRouter>
 )
